960d651fbaa145956592cded1a7954f2f3dd776d
Daniel Schürmann <daschuer@gmx.de>
2013-06-11 12:19:09 +0200

    Allow Loops up to 100 with Keil MDK-ARM compiler

--- loop.h
+++ loop.h
@@ -3,9 +3,6 @@
 #endif
 
 #if(LOOP_START < (LOOP_END))
-
-    MACRO(LOOP_START)
-
     #if((LOOP_START / 10) == 0)
         #undef LOOP_START10
         #define LOOP_START10
@@ -41,69 +38,8 @@
         #define LOOP_START10 10
     #endif
 
-
-    #if((LOOP_START % 10) == 0)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 1)
-    #elif((LOOP_START % 10) == 1)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 2)
-    #elif((LOOP_START % 10) == 2)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 3)
-    #elif((LOOP_START % 10) == 3)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 4)
-    #elif((LOOP_START % 10) == 4)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 5)
-    #elif((LOOP_START % 10) == 5)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 6)
-    #elif((LOOP_START % 10) == 6)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 7)
-    #elif((LOOP_START % 10) == 7)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 8)
-    #elif((LOOP_START % 10) == 8)
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 9)
-    #elif((LOOP_START % 10) == 9)
-        #if((LOOP_START / 10) == 0 )
-            #undef LOOP_START10
-            #define LOOP_START10 1
-        #elif((LOOP_START / 10) == 1)
-            #undef LOOP_START10
-            #define LOOP_START10 2
-        #elif((LOOP_START / 10) == 2)
-            #undef LOOP_START10
-            #define LOOP_START10 3
-        #elif((LOOP_START / 10) == 3)
-            #undef LOOP_START10
-            #define LOOP_START10 4
-        #elif((LOOP_START / 10) == 4)
-            #undef LOOP_START10
-            #define LOOP_START10 5
-        #elif((LOOP_START / 10) == 5)
-            #undef LOOP_START10
-            #define LOOP_START10 6
-        #elif((LOOP_START / 10) == 6)
-            #undef LOOP_START10
-            #define LOOP_START10 7
-        #elif((LOOP_START / 10) == 7)
-            #undef LOOP_START10
-            #define LOOP_START10 8
-        #elif((LOOP_START / 10) == 8)
-            #undef LOOP_START10
-            #define LOOP_START10 9
-        #elif((LOOP_START / 10) == 9)
-            #undef LOOP_START10
-            #define LOOP_START10 10
-        #endif
-        #undef LOOP_START
-        #define LOOP_START JOIN(LOOP_START10, 0)
+    #include "loop1.h"
+    #if(LOOP_START < (LOOP_END))
+        #include "loop.h"
     #endif
-
-    #include "loop.h"
 #endif
